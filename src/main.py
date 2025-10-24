e51ef35496eb52901f9c27f2cdba686433a61f03
wilder898 <wilder898@gmail.com>
2025-10-24 16:44:25 -0500

    Initial commit

---
+++ src/main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+import uvicorn
+
+# Crear la instancia de FastAPI
+app = FastAPI()
+
+# Lista de peliculas
+
+lista_peliculas = [
+    {"id": 1, "nombre": "Avatar", "año": 2012, "director": "James Cameron" },
+]
+
+contador_id = 2
+
+@app.get("/")
+def root():
+    return {"mensaje": "¡Bienvenido a mi API con FastAPI!"}
+
+# Ejecutar la aplicación
+# uvicorn main:app --reload
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
