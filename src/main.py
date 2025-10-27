8a5aa29d1511bc8ae44658e4e77a75119e7dfa54
Cristian <179995201+CrisLmon@users.noreply.github.com>
2025-10-27 18:19:48 -0500

    creacion de endpoint 'Obtener pelicula por id'

--- src/main.py
+++ src/main.py
@@ -16,6 +16,13 @@ contador_id = 2
 def root():
     return {"mensaje": "¡Bienvenido a mi API con FastAPI!"}
 
+@app.get("/peliculas/{pelicula_id}")
+def obtener_por_id(pelicula_id: int):
+    for pelicula in lista_peliculas:
+        if pelicula["id"] == pelicula_id:
+            return {"pelicula": pelicula}
+    return {"mensaje": f"No existe pelicula con el id {pelicula_id}"}
+
 # Ejecutar la aplicación
 # uvicorn main:app --reload
 if __name__ == "__main__":
